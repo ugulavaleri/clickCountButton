7540d30879eae09ddb5b710321a62b12854a9554
ugulavaleri <leri.ugulava1@gmail.com>
2023-05-17 00:36:07 +0400

    created one function for two button

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -3,20 +3,21 @@ import { useState } from "react";
 
 const DinamicButton = () => {
     const [count, setCount] = useState(0);
+
+    const handleClick = (value) => {
+        if (value === "increase") {
+            setCount(count + 1);
+        } else {
+            count > 0 ? setCount(count - 1) : setCount(0);
+        }
+    };
+
     return (
         <div className="container">
             <div className="buttonsBox">
-                <button
-                    onClick={
-                        count > 0
-                            ? () => setCount(count - 1)
-                            : () => setCount(0)
-                    }
-                >
-                    -
-                </button>
+                <button onClick={() => handleClick("decrease")}>-</button>
                 <button>i was Clicked {count} times</button>
-                <button onClick={() => setCount(count + 1)}>+</button>
+                <button onClick={() => handleClick("increase")}>+</button>
             </div>
         </div>
     );
